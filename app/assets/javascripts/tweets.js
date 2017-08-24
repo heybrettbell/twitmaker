0b7514f6915b95acfd7261a199deccc5863dc5c6
heybrettbell <brett@grassrootsonline.ca>
2017-08-24 17:33:51 -0400

    Added response data handling, controller mods

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,58 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+document.addEventListener("DOMContentLoaded", function() {
+
+var submitTweet = document.querySelector('#new_tweet');
+var allTweets = document.querySelector('.tweets');
+var button = document.querySelector('#create-tweet');
+
+submitTweet.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+    var request = $.ajax ({
+      url: this.action,
+      method: this.method,
+      data: $(this).serialize(),
+      dataType: 'json'
+    }).done(function(responseData){
+      console.log(responseData);
+      var li = document.createElement('li');
+      var p = document.createElement('p');
+      li.className = 'tweet';
+      p.innerText = responseData.message;
+      li.append(p);
+
+      var postTime = document.createElement('time');
+      postTime.innerText = new Date();
+      li.append(postTime);
+      allTweets.prepend(li);
+
+      submitTweet.reset();
+
+      button.removeAttribute('disabled');
+      button.removeAttribute('data-disable-with');
+    });
+  });
+});
+
+//       var inputElement = document.createElement('input');
+//         inputElement.setAttribute('type', 'checkbox');
+//         inputElement.setAttribute('value', data.id);
+//         inputElement.setAttribute('name', 'todos[]');
+//         inputElement.className = 'todo-complete';
+//
+// // c. Create a label element with the task field
+// var labelElement = document.createElement('label');
+// labelElement.innerHTML = data.task;
+//
+// var newElement = document.createElement('li');
+// var newP = document.createElement('p');
+// newP.innerText = data.message;
+// newElement.append(newP);
+// var newTime = document.createElement('time');
+// newTime.innerText = new Date();
+// newElement.append(newTime);
+// newElement.classList = 'tweet';
+// allTweets.prepend(li);
+//
+// newTweet.reset();
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,14 +5,22 @@ class TweetsController < ApplicationController
   end
 
   def create
-    @tweet = Tweet.new(tweet_params)
+      @tweet = Tweet.new(tweet_params)
 
-    if @tweet.save
-      redirect_to tweets_path
-    else
-      render :index
+      if @tweet.save
+        # redirect_to tweets_path
+
+        respond_to do |format|
+          format.html do
+            redirect_to tweets_path
+          end
+
+          format.json { render json: @tweet }
+        end
+      else
+        render :index
+      end
     end
-  end
 
   def destroy
   end
